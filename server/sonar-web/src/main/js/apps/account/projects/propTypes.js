SonarSource/sonarqube

SONAR-7229 Create new Projects page for My Account space

server/sonar-web/src/main/js/apps/account/projects/propTypes.js
@@ -1,0 +1,34 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import React from 'react';
+
+const { shape, string, array, arrayOf } = React.PropTypes;
+
+export const projectType = shape({
+  id: string.isRequired,
+  key: string.isRequired,
+  name: string.isRequired,
+  lastAnalysisDate: string,
+  description: string,
+  links: array.isRequired,
+  qualityGateStatus: string
+});
+
+export const projectsListType = arrayOf(projectType);

SonarSource/sonarqube-webapp

server/sonar-web/src/main/js/apps/account/projects/propTypes.js
@@ -1,0 +1,34 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import React from 'react';
+
+const { shape, string, array, arrayOf } = React.PropTypes;
+
+export const projectType = shape({
+  id: string.isRequired,
+  key: string.isRequired,
+  name: string.isRequired,
+  lastAnalysisDate: string,
+  description: string,
+  links: array.isRequired,
+  qualityGate: string
+});
+
+export const projectsListType = arrayOf(projectType);