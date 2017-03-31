biruce-ci/sonarqube

SONAR-8990 Add the list of members of an organization

server/sonar-web/src/main/js/apps/organizations/components/OrganizationMembersContainer.js
@@ -17,52 +17,26 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-@import (reference) "../variables";
-@import (reference) "../mixins";
+import { connect } from 'react-redux';
+import OrganizationMembers from './OrganizationMembers';
+import {
+  getOrganizationByKey,
+  getOrganizationMembersLogins,
+  getUsersByLogins,
+  getOrganizationMembersState
+} from '../../../store/rootReducer';
+import { fetchOrganizationMembers, fetchMoreOrganizationMembers } from '../store/actions';
 
-.search-box {
-  position: relative;
-  font-size: 0;
-  white-space: nowrap;
-}
+const mapStateToProps = (state, ownProps) => {
+  const { organizationKey } = ownProps.params;
+  const memberLogins = getOrganizationMembersLogins(state, organizationKey);
+  return {
+    members: getUsersByLogins(state, memberLogins),
+    organization: getOrganizationByKey(state, organizationKey),
+    state: getOrganizationMembersState(state, organizationKey)
+  };
+};
 
-.search-box-input {
-  vertical-align: middle;
-  width: 250px;
-  border: none !important;
-  font-size: @baseFontSize;
-
-  & ~ .note {
-    opacity: 0;
-    transition: opacity 0.3s ease;
-  }
-
-  &.touched ~ .note {
-    opacity: 1;
-  }
-}
-
-.search-box-submit {
-  display: inline-block;
-  vertical-align: middle;
-
-  .icon-search:before {
-    color: @secondFontColor;
-    font-size: @iconSmallFontSize;
-  }
-
-  .icon-search-new {
-    position: relative;
-    top: 1px;
-  }
-}
-
-.search-box-input-note {
-  position: absolute;
-  top: 100%;
-  left: 0;
-  line-height: 1;
-  color: #777;
-  font-size: @smallFontSize;
-  white-space: nowrap;
-}
+export default connect(mapStateToProps, { fetchOrganizationMembers, fetchMoreOrganizationMembers })(
+  OrganizationMembers
+);

server/sonar-web/src/main/js/apps/users/components/__tests__/MembersListItem-test.js
@@ -17,52 +17,30 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-@import (reference) "../variables";
-@import (reference) "../mixins";
+import React from 'react';
+import { shallow } from 'enzyme';
+import MembersListItem from '../MembersListItem';
 
-.search-box {
-  position: relative;
-  font-size: 0;
-  white-space: nowrap;
-}
+const organization = { key: 'foo', name: 'Foo' };
+const admin = { login: 'admin', name: 'Admin Istrator', avatar: '', groupCount: 3 };
+const john = { login: 'john', name: 'John Doe', avatar: '7daf6c79d4802916d83f6266e24850af', groupCount: 1 };
 
-.search-box-input {
-  vertical-align: middle;
-  width: 250px;
-  border: none !important;
-  font-size: @baseFontSize;
+it('should not render actions and groups for non admin', () => {
+  const wrapper = shallow(
+    <MembersListItem
+      organization={organization}
+      user={john}
+    />
+  );
+  expect(wrapper).toMatchSnapshot();
+});
 
-  & ~ .note {
-    opacity: 0;
-    transition: opacity 0.3s ease;
-  }
-
-  &.touched ~ .note {
-    opacity: 1;
-  }
-}
-
-.search-box-submit {
-  display: inline-block;
-  vertical-align: middle;
-
-  .icon-search:before {
-    color: @secondFontColor;
-    font-size: @iconSmallFontSize;
-  }
-
-  .icon-search-new {
-    position: relative;
-    top: 1px;
-  }
-}
-
-.search-box-input-note {
-  position: absolute;
-  top: 100%;
-  left: 0;
-  line-height: 1;
-  color: #777;
-  font-size: @smallFontSize;
-  white-space: nowrap;
-}
+it('should render actions and groups for admin', () => {
+  const wrapper = shallow(
+    <MembersListItem
+      organization={{ ...organization, canAdmin: true }}
+      user={admin}
+    />
+  );
+  expect(wrapper).toMatchSnapshot();
+});

server/sonar-web/src/main/js/apps/organizations/components/__tests__/OrganizationMembers-test.js
@@ -1,0 +1,57 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import React from 'react';
+import { shallow } from 'enzyme';
+import OrganizationMembers from '../OrganizationMembers';
+
+const organization = { key: 'foo', name: 'Foo' };
+const members = [
+  { login: 'admin', name: 'Admin Istrator', avatar: '', groupCount: 3 },
+  { login: 'john', name: 'John Doe', avatar: '7daf6c79d4802916d83f6266e24850af', groupCount: 1 }
+];
+const state = { total: members.length };
+const fetchOrganizationMembers = jest.fn();
+const fetchMoreOrganizationMembers = jest.fn();
+
+it('should not render actions for non admin', () => {
+  const wrapper = shallow(
+    <OrganizationMembers
+      organization={organization}
+      members={members}
+      state={state}
+      fetchOrganizationMembers={fetchOrganizationMembers}
+      fetchMoreOrganizationMembers={fetchMoreOrganizationMembers}
+    />
+  );
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render actions for admin', () => {
+  const wrapper = shallow(
+    <OrganizationMembers
+      organization={{ ...organization, canAdmin: true }}
+      members={members}
+      state={{ ...state, loading: true }}
+      fetchOrganizationMembers={fetchOrganizationMembers}
+      fetchMoreOrganizationMembers={fetchMoreOrganizationMembers}
+    />
+  );
+  expect(wrapper).toMatchSnapshot();
+});

SonarSource/sonarqube-webapp

server/sonar-web/src/main/js/apps/organizations/components/OrganizationMembersContainer.js
@@ -17,52 +17,26 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-@import (reference) "../variables";
-@import (reference) "../mixins";
+import { connect } from 'react-redux';
+import OrganizationMembers from './OrganizationMembers';
+import {
+  getOrganizationByKey,
+  getOrganizationMembersLogins,
+  getUsersByLogins,
+  getOrganizationMembersState
+} from '../../../store/rootReducer';
+import { fetchOrganizationMembers, fetchMoreOrganizationMembers } from '../actions';
 
-.search-box {
-  position: relative;
-  font-size: 0;
-  white-space: nowrap;
-}
+const mapStateToProps = (state, ownProps) => {
+  const { organizationKey } = ownProps.params;
+  const memberLogins = getOrganizationMembersLogins(state, organizationKey);
+  return {
+    members: getUsersByLogins(state, memberLogins),
+    organization: getOrganizationByKey(state, organizationKey),
+    status: getOrganizationMembersState(state, organizationKey)
+  };
+};
 
-.search-box-input {
-  vertical-align: middle;
-  width: 250px;
-  border: none !important;
-  font-size: @baseFontSize;
-
-  & ~ .note {
-    opacity: 0;
-    transition: opacity 0.3s ease;
-  }
-
-  &.touched ~ .note {
-    opacity: 1;
-  }
-}
-
-.search-box-submit {
-  display: inline-block;
-  vertical-align: middle;
-
-  .icon-search:before {
-    color: @secondFontColor;
-    font-size: @iconSmallFontSize;
-  }
-
-  .icon-search-new {
-    position: relative;
-    top: 1px;
-  }
-}
-
-.search-box-input-note {
-  position: absolute;
-  top: 100%;
-  left: 0;
-  line-height: 1;
-  color: #777;
-  font-size: @smallFontSize;
-  white-space: nowrap;
-}
+export default connect(mapStateToProps, { fetchOrganizationMembers, fetchMoreOrganizationMembers })(
+  OrganizationMembers
+);

server/sonar-web/src/main/js/apps/organizations/components/__tests__/MembersListItem-test.js
@@ -17,52 +17,30 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-@import (reference) "../variables";
-@import (reference) "../mixins";
+import React from 'react';
+import { shallow } from 'enzyme';
+import MembersListItem from '../MembersListItem';
 
-.search-box {
-  position: relative;
-  font-size: 0;
-  white-space: nowrap;
-}
+const organization = { key: 'foo', name: 'Foo' };
+const admin = { login: 'admin', name: 'Admin Istrator', avatar: '', groupCount: 3 };
+const john = { login: 'john', name: 'John Doe', avatar: '7daf6c79d4802916d83f6266e24850af', groupCount: 1 };
 
-.search-box-input {
-  vertical-align: middle;
-  width: 250px;
-  border: none !important;
-  font-size: @baseFontSize;
+it('should not render actions and groups for non admin', () => {
+  const wrapper = shallow(
+    <MembersListItem
+      organization={organization}
+      member={john}
+    />
+  );
+  expect(wrapper).toMatchSnapshot();
+});
 
-  & ~ .note {
-    opacity: 0;
-    transition: opacity 0.3s ease;
-  }
-
-  &.touched ~ .note {
-    opacity: 1;
-  }
-}
-
-.search-box-submit {
-  display: inline-block;
-  vertical-align: middle;
-
-  .icon-search:before {
-    color: @secondFontColor;
-    font-size: @iconSmallFontSize;
-  }
-
-  .icon-search-new {
-    position: relative;
-    top: 1px;
-  }
-}
-
-.search-box-input-note {
-  position: absolute;
-  top: 100%;
-  left: 0;
-  line-height: 1;
-  color: #777;
-  font-size: @smallFontSize;
-  white-space: nowrap;
-}
+it('should render actions and groups for admin', () => {
+  const wrapper = shallow(
+    <MembersListItem
+      organization={{ ...organization, canAdmin: true }}
+      member={admin}
+    />
+  );
+  expect(wrapper).toMatchSnapshot();
+});

server/sonar-web/src/main/js/apps/organizations/components/__tests__/OrganizationMembers-test.js
@@ -1,0 +1,57 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import React from 'react';
+import { shallow } from 'enzyme';
+import OrganizationMembers from '../OrganizationMembers';
+
+const organization = { key: 'foo', name: 'Foo' };
+const members = [
+  { login: 'admin', name: 'Admin Istrator', avatar: '', groupCount: 3 },
+  { login: 'john', name: 'John Doe', avatar: '7daf6c79d4802916d83f6266e24850af', groupCount: 1 }
+];
+const status = { total: members.length };
+const fetchOrganizationMembers = jest.fn();
+const fetchMoreOrganizationMembers = jest.fn();
+
+it('should not render actions for non admin', () => {
+  const wrapper = shallow(
+    <OrganizationMembers
+      organization={organization}
+      members={members}
+      status={status}
+      fetchOrganizationMembers={fetchOrganizationMembers}
+      fetchMoreOrganizationMembers={fetchMoreOrganizationMembers}
+    />
+  );
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render actions for admin', () => {
+  const wrapper = shallow(
+    <OrganizationMembers
+      organization={{ ...organization, canAdmin: true }}
+      members={members}
+      status={{ ...status, loading: true }}
+      fetchOrganizationMembers={fetchOrganizationMembers}
+      fetchMoreOrganizationMembers={fetchMoreOrganizationMembers}
+    />
+  );
+  expect(wrapper).toMatchSnapshot();
+});