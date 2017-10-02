SonarSource/sonarqube

SONAR-1330 Allow only authorized actions on the Quality Profiles page

server/sonar-web/src/main/js/apps/projectQualityProfiles/App.tsx
@@ -70,8 +70,8 @@ export default class QualityProfiles extends React.PureComponent<Props, State> {
     const { component } = this.props;
     const organization = this.props.customOrganizations ? component.organization : undefined;
     Promise.all([
-      searchQualityProfiles({ organization }),
-      searchQualityProfiles({ organization, projectKey: component.key })
+      searchQualityProfiles({ organization }).then(r => r.profiles),
+      searchQualityProfiles({ organization, projectKey: component.key }).then(r => r.profiles)
     ]).then(
       ([allProfiles, profiles]) => {
         if (this.mounted) {

SonarSource/sonarqube-webapp

server/sonar-web/src/main/js/apps/projectQualityProfiles/App.tsx
@@ -70,8 +70,8 @@ export default class QualityProfiles extends React.PureComponent<Props, State> {
     const { component } = this.props;
     const organization = this.props.customOrganizations ? component.organization : undefined;
     Promise.all([
-      searchQualityProfiles({ organization }),
-      searchQualityProfiles({ organization, project: component.key })
+      searchQualityProfiles({ organization }).then(r => r.profiles),
+      searchQualityProfiles({ organization, project: component.key }).then(r => r.profiles)
     ]).then(
       ([allProfiles, profiles]) => {
         if (this.mounted) {