pzhao12teste/sonarqube

SONAR-10345 Add webhooks management actions
* SONAR-10345 Add the webhooks create/update form
* SONAR-10345 Add the webhooks delete action
* SONAR-10345 Add fields validation on webhook create page

server/sonar-web/src/main/js/api/webhooks.ts
@@ -17,13 +17,34 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import { getJSON } from '../helpers/request';
+import { getJSON, post, postJSON } from '../helpers/request';
 import throwGlobalError from '../app/utils/throwGlobalError';
 import { Webhook } from '../app/types';
+
+export function createWebhook(data: {
+  name: string;
+  organization: string | undefined;
+  project?: string;
+  url: string;
+}): Promise<{ webhook: Webhook }> {
+  return postJSON('/api/webhooks/create', data).catch(throwGlobalError);
+}
+
+export function deleteWebhook(data: { key: string }): Promise<void | Response> {
+  return post('/api/webhooks/delete', data).catch(throwGlobalError);
+}
 
 export function searchWebhooks(data: {
   organization: string | undefined;
   project?: string;
 }): Promise<{ webhooks: Webhook[] }> {
   return getJSON('/api/webhooks/search', data).catch(throwGlobalError);
+}
+
+export function updateWebhook(data: {
+  key: string;
+  name: string;
+  url: string;
+}): Promise<void | Response> {
+  return post('/api/webhooks/update', data).catch(throwGlobalError);
 }

SonarSource/sonarqube-webapp

server/sonar-web/src/main/js/api/webhooks.ts
@@ -17,13 +17,34 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import { getJSON } from '../helpers/request';
+import { getJSON, post, postJSON } from '../helpers/request';
 import throwGlobalError from '../app/utils/throwGlobalError';
 import { Webhook } from '../app/types';
+
+export function createWebhook(data: {
+  name: string;
+  organization: string | undefined;
+  project?: string;
+  url: string;
+}): Promise<{ webhook: Webhook }> {
+  return postJSON('/api/webhooks/create', data).catch(throwGlobalError);
+}
+
+export function deleteWebhook(data: { webhook: string }): Promise<void | Response> {
+  return post('/api/webhooks/delete', data).catch(throwGlobalError);
+}
 
 export function searchWebhooks(data: {
   organization: string | undefined;
   project?: string;
 }): Promise<{ webhooks: Webhook[] }> {
-  return getJSON('/api/webhooks/search', data).catch(throwGlobalError);
+  return getJSON('/api/webhooks/list', data).catch(throwGlobalError);
+}
+
+export function updateWebhook(data: {
+  webhook: string;
+  name: string;
+  url: string;
+}): Promise<void | Response> {
+  return post('/api/webhooks/update', data).catch(throwGlobalError);
 }