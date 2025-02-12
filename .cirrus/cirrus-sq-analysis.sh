dcd788842729725d13119fecec6fccb5375da0ec
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2025-02-12 11:11:00 +0100

    SC-20089 Fix SQS sonar analysis

--- .cirrus/cirrus-sq-analysis.sh
+++ .cirrus/cirrus-sq-analysis.sh
@@ -17,10 +17,14 @@ if [ -n "${GITHUB_BASE_BRANCH:-}" ]; then
   git fetch origin "${GITHUB_BASE_BRANCH}"
 fi
 
+
+PROJECT_VERSION=$(jq -r .version "apps/sq-server/package.json")
+
 scanner_params=(
     "-DbuildNumber=${BUILD_NUMBER}"
     "-Dsonar.projectKey=sonarqube-webapp"
     "-Dsonar.projectName=SonarQube Webapp"
+    "-Dsonar.projectVersion=${PROJECT_VERSION}"
     "-Dsonar.host.url=${SONAR_HOST_URL}"
     "-Dsonar.token=${SONAR_TOKEN}"
     "-Dsonar.analysis.buildNumber=${BUILD_NUMBER}"
@@ -31,10 +35,15 @@ scanner_params=(
     "-Dsonar.javascript.lcov.reportPaths=apps/sq-server/build/reports/coverage/lcov.info,coverage/libs/cross-domain/sq-server-shared/lcov.info"
     "-Dsonar.sources=apps/sq-server/src,libs/addons/src,libs/cross-domain/sq-server-shared/src"
     "-Dsonar.exclusions=**/__tests__/**"
-    "-Dsonar.cpd.exclusions=**/src/**/*Legacy.*
+    "-Dsonar.cpd.exclusions=**/src/**/mocks/**,
+                            **/src/**/*Legacy.*,
+                            **/src/**/l10n/default.ts"
     "-Dsonar.tests=apps/sq-server/,libs/cross-domain/sq-server-shared/"
     "-Dsonar.test.inclusions=**/__tests__/**"
     "-Dsonar.coverage.exclusions=**/__mocks__/**,
+                                **/config/**,
+                                **/*.js,
+                                **/*.mjs,
                                 **/@types/**,
                                 **/components/icons/**,
                                 **/components/visual-components/**,
@@ -51,7 +60,6 @@ scanner_params=(
                                 **/helpers/testMocks.ts,
                                 **/helpers/keyboard.ts,
                                 **/helpers/cookies.ts,
-                                **/*Legacy.*
-                                ")
+                                **/*Legacy.*")
 
 sonar-scanner "${scanner_params[@]}"
