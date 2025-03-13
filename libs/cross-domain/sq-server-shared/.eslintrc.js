13abd42cbe718d5056a47c7be38793ab306791b1
David Cho-Lerat <david.cho-lerat@sonarsource.com>
2025-03-13 15:46:11 +0100

    SC-21864 Replicate parserOptions for VS Code eslint plugin

--- libs/cross-domain/sq-server-shared/.eslintrc.js
+++ libs/cross-domain/sq-server-shared/.eslintrc.js
@@ -11,4 +11,18 @@ module.exports = {
     'script/**/*',
     'config/**/*',
   ],
+
+  // WARNING: the section below is not strictly needed for ESLint to work - it's a duplicate of what
+  // is already in .eslintrc.rules.js, and the eslint command works fine without it - but not
+  // replicating it here confuses the VS Code eslint plugin, so please don't remove it!
+  overrides: [
+    {
+      files: ['**/*.{ts,tsx}'],
+
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
+  //
 };
