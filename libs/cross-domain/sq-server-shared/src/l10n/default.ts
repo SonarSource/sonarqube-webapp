5981629841d94686ed7c71c3ddc33d0325119083
Bill Nottingham <bill.nottingham@sonarsource.com>
2025-03-19 18:27:54 -0400

    SQRP-320 Fix wrong l10n key in risk score header

--- libs/cross-domain/sq-server-shared/src/l10n/default.ts
+++ libs/cross-domain/sq-server-shared/src/l10n/default.ts
@@ -1654,8 +1654,9 @@ export const defaultMessages = {
   'dependencies.risks.severity.INFO': 'Info',
   'dependencies.risks.severity.INFO.description': 'Confirmed false positive or withdrawn',
   'dependencies.risks.cvss_score': 'CVSS {score}',
-  'dependencies.risks.epss_score': 'EPSS probability',
   'dependencies.risks.no_cvss_score': 'No CVSS score',
+  'dependencies.risks.cvss_title': 'CVSS score',
+  'dependencies.risks.epss_title': 'EPSS probability',
 
   'dependencies.risk.how.title': 'Update the dependency',
   'dependencies.risk.how.body':
