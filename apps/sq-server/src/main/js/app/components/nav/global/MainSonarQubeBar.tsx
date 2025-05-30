397d7ead4486b0315b507920f02fb98a762b9ad2
David Cho-Lerat <david.cho-lerat@sonarsource.com>
2025-05-30 12:41:01 +0200

    SONAR-24877 Fix Custom logo size not constrained in MainSonarQubeBar

--- apps/sq-server/src/main/js/app/components/nav/global/MainSonarQubeBar.tsx
+++ apps/sq-server/src/main/js/app/components/nav/global/MainSonarQubeBar.tsx
@@ -20,28 +20,45 @@
 
 import { LogoSize } from '@sonarsource/echoes-react';
 import { useContext } from 'react';
+import { useIntl } from 'react-intl';
 import { Image } from '~adapters/components/common/Image';
 import { MainAppBar } from '~design-system';
 import { SonarQubeProductLogo } from '~sq-server-commons/components/branding/SonarQubeProductLogo';
 import { AppStateContext } from '~sq-server-commons/context/app-state/AppStateContext';
-import { translate } from '~sq-server-commons/helpers/l10n';
 import { GlobalSettingKeys } from '~sq-server-commons/types/settings';
 
 const DEFAULT_CUSTOM_LOGO_WIDTH_IN_PX = 100;
+const MAX_LOGO_HEIGHT_IN_PX = 40;
+const MAX_LOGO_WIDTH_IN_PX = 150;
 
 export function LogoWithAriaText() {
   const { settings } = useContext(AppStateContext);
+  const intl = useIntl();
+
   const customLogoUrl = settings[GlobalSettingKeys.LogoUrl];
-  const customLogoWidth = settings[GlobalSettingKeys.LogoWidth] ?? DEFAULT_CUSTOM_LOGO_WIDTH_IN_PX;
+
+  const customLogoWidth = parseInt(
+    settings[GlobalSettingKeys.LogoWidth] ?? `${DEFAULT_CUSTOM_LOGO_WIDTH_IN_PX}`,
+    10,
+  );
 
   const title = customLogoUrl
-    ? translate('layout.nav.home_logo_alt')
-    : translate('layout.nav.home_sonarqube_logo_alt');
+    ? intl.formatMessage({ id: 'layout.nav.home_logo_alt' })
+    : intl.formatMessage({ id: 'layout.nav.home_sonarqube_logo_alt' });
 
   return (
     <div aria-label={title} role="img">
       {customLogoUrl ? (
-        <Image alt={title} src={customLogoUrl} width={customLogoWidth} />
+        <Image
+          alt={title}
+          src={customLogoUrl}
+          style={{
+            maxHeight: `${MAX_LOGO_HEIGHT_IN_PX}px`,
+            maxWidth: `${MAX_LOGO_WIDTH_IN_PX}px`,
+            objectFit: 'contain',
+          }}
+          width={Math.min(customLogoWidth, MAX_LOGO_WIDTH_IN_PX)}
+        />
       ) : (
         <SonarQubeProductLogo hasText size={LogoSize.Medium} />
       )}
