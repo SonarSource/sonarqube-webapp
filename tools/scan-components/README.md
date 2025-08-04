f2bba7e3f8cc50534e39bde1b01b7d38e76dbbbc
Jay Davis <jeremy.davis@sonarsource.com>
2025-08-04 16:59:44 +0200

    SC-30211 Add script to collect component usage

---
+++ tools/scan-components/README.md
@@ -0,0 +1,19 @@
+# Scan Components
+
+This script uses react-scanner to count the uses of components in the webapp.
+
+It's used to track our adoption of @sonarsource/echoes-react.
+
+## Run
+
+Simply run
+
+```sh
+yarn scan-components
+```
+
+This generates a CSV file here: `.componentscan/components.csv`.
+
+This CSV data is meant to suit a dedicated spreadsheet to track adoption over time: `State of Echoes adoption 25Q3-4`.
+
+Reach out to the FEE squad if you want access.
---
+++ tools/scan-components/index.js
@@ -0,0 +1,83 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import fs from 'fs';
+import fsp from 'fs/promises';
+import scanner from 'react-scanner';
+import { componentMap } from './componentMap.js';
+
+const config = {
+  rootDir: '.',
+  crawlFrom: '.',
+  exclude: ['__tests__'],
+  globs: ['**/*.tsx'],
+  includeSubComponents: true,
+  processors: ['count-components'],
+};
+
+await run();
+
+async function run() {
+  const echoesComponents = await countComponentsFrom(
+    'echoes',
+    '@sonarsource/echoes-react',
+    'Internal',
+  );
+  const internalComponents = await countComponentsFrom('internal', /^~/);
+  const relativeComponents = await countComponentsFrom('relative', /^\.\.?\//);
+
+  await writeOut([...echoesComponents, ...internalComponents, ...relativeComponents]);
+}
+
+async function countComponentsFrom(sourceName, source, covered) {
+  const output = await scanner.run({ ...config, importedFrom: source });
+
+  const merged = Object.keys(output).reduce(
+    (result, key) => {
+      // Combine echoes "Icon" components into a single category, exclude ButtonIcon
+      if (sourceName === 'echoes' && key.includes('Icon') && key !== 'ButtonIcon') {
+        result.Icons = result.Icons + output[key];
+      } else {
+        result[key] = output[key];
+      }
+
+      return result;
+    },
+    { Icons: 0 },
+  );
+
+  return Object.keys(merged).map((key) => ({
+    component: key,
+    count: merged[key],
+    match: covered ?? componentMap[key],
+    source: sourceName,
+  }));
+}
+
+async function writeOut(data) {
+  const csvRows = data
+    .map(({ component, count, match, source }) => `"${component}","${source}",${count},"${match}"`)
+    .join('\n');
+
+  if (!fs.existsSync('.componentscan')) {
+    await fsp.mkdir('.componentscan');
+  }
+  await fsp.writeFile('.componentscan/components.csv', csvRows, 'utf8');
+}
