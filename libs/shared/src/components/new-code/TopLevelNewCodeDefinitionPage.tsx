d9d1f49e96d2b7e23ccb34495f7300d473bac35a
David Cho-Lerat <david.cho-lerat@sonarsource.com>
2025-10-02 14:45:30 +0200

    SC-32262 Migrate Organization New Code Page to New Sidebar and Layout (#3286)
    GitOrigin-RevId: 01bcc3e6dc4e643577d87cbc624bd15206fba4f0

--- libs/shared/src/components/new-code/TopLevelNewCodeDefinitionPage.tsx
+++ libs/shared/src/components/new-code/TopLevelNewCodeDefinitionPage.tsx
@@ -37,7 +37,6 @@ import {
 } from '@sonarsource/echoes-react';
 import { clone } from 'lodash';
 import { useCallback, useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet-async';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { SharedDocLink, useSharedDocUrl } from '~adapters/helpers/docs';
 import {
@@ -56,14 +55,16 @@ function isValidDefinition(definition: NewCodeDefinition): boolean {
   switch (definition.type) {
     case NewCodeDefinitionType.PreviousVersion:
       return true;
+
     case NewCodeDefinitionType.NumberOfDays:
       return isValidDays(definition.value);
+
     default:
       return false;
   }
 }
 
-export default function TopLevelNewCodeDefinitionPage() {
+export function TopLevelNewCodeDefinitionPage() {
   const intl = useIntl();
   const { data, isLoading } = useNewCodeDefinitionQuery();
   const docUrl = useSharedDocUrl();
@@ -76,9 +77,11 @@ export default function TopLevelNewCodeDefinitionPage() {
   });
 
   const [newCodeDefinition, setNewCodeDefinition] = useState<NewCodeDefinition | undefined>(data);
+
   const [numberOfDays, setNumberOfDays] = useState(
     data?.type === NewCodeDefinitionType.NumberOfDays ? data.value : '30',
   );
+
   const [selectedType, setSelectedType] = useState<NewCodeDefinitionType>(
     data?.type || NewCodeDefinitionType.PreviousVersion,
   );
@@ -113,6 +116,7 @@ export default function TopLevelNewCodeDefinitionPage() {
 
   const handleDaysChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const days = e.target.value;
+
     const definition: NewCodeDefinition = {
       type: NewCodeDefinitionType.NumberOfDays,
       value: days,
@@ -147,190 +151,165 @@ export default function TopLevelNewCodeDefinitionPage() {
   const isSaveButtonDisabled = isPending || !hasChanged || !newCodeDefinition?.isValid;
 
   return (
-    <>
-      <Helmet defer={false} title={intl.formatMessage({ id: 'new_code_definition.page.title' })} />
-
-      {/* Main Content Card */}
-      <div className="sw-max-w-abs-800">
-        <div className="sw-mb-8">
-          <Heading as="h1" className="sw-mb-4">
-            <FormattedMessage id="new_code_definition.page.title" />
+    <Spinner isLoading={isLoading}>
+      <form className="sw-space-y-8">
+        {/* New Code Definition Selection */}
+        <div className="sw-space-y-4">
+          <Heading as="h2" size="medium">
+            <FormattedMessage id="new_code_definition.question" />
           </Heading>
-          <Text>
-            <FormattedMessage
-              id="new_code.page.description"
-              values={{
-                br: () => <br />,
-                url: (
-                  <Link
-                    enableOpenInNewTab
-                    highlight={LinkHighlight.CurrentColor}
-                    to={docUrl(SharedDocLink.NewCodeDefinition)}
-                  >
-                    <FormattedMessage id="new_code.page.description.documentation_link" />
-                  </Link>
-                ),
-              }}
-            />
-          </Text>
-        </div>
 
-        <Spinner isLoading={isLoading}>
-          <form className="sw-space-y-8">
-            {/* New Code Definition Selection */}
-            <div className="sw-space-y-4">
-              <Heading as="h2" size="medium">
-                <FormattedMessage id="new_code_definition.question" />
-              </Heading>
-              <div className="sw-space-y-6">
-                <div>
-                  <RadioButtonGroup
-                    id="new-code-definition-type"
-                    onChange={(value) => {
-                      handleTypeChange(value as NewCodeDefinitionType);
-                    }}
-                    options={[
-                      {
-                        label: intl.formatMessage({ id: 'new_code_definition.previous_version' }),
-                        value: NewCodeDefinitionType.PreviousVersion,
-                        helpText: (
-                          <>
-                            <div>
-                              <Text isSubtle size={TextSize.Small}>
-                                {intl.formatMessage({
-                                  id: 'new_code_definition.previous_version.description',
-                                })}{' '}
-                                {intl.formatMessage(
-                                  {
-                                    id: 'new_code_definition.previous_version.usecase',
-                                  },
-                                  {
-                                    b: (text) => (
-                                      <Text isHighlighted isSubtle size={TextSize.Small}>
-                                        {text}
-                                      </Text>
-                                    ),
-                                  },
-                                )}
-                              </Text>
-                            </div>
-                            <Divider className="sw-mt-4 sw-mb-2 -sw-ml-6" />
-                          </>
-                        ),
-                      },
-                      {
-                        label: intl.formatMessage({ id: 'new_code_definition.number_days' }),
-                        value: NewCodeDefinitionType.NumberOfDays,
-                        helpText: (
-                          <div className="sw-space-y-3">
-                            <Text isSubtle size={TextSize.Small}>
-                              {intl.formatMessage({
-                                id: 'new_code_definition.number_days.description',
-                              })}{' '}
-                              {intl.formatMessage(
-                                {
-                                  id: 'new_code_definition.number_days.usecase',
-                                },
-                                {
-                                  b: (text) => (
-                                    <Text isHighlighted isSubtle size={TextSize.Small}>
-                                      {text}
-                                    </Text>
-                                  ),
-                                },
-                              )}
-                            </Text>
-                            {selectedType === NewCodeDefinitionType.NumberOfDays && (
-                              <div className="sw-space-y-2">
-                                <div className="sw-max-w-600">
-                                  <TextInput
-                                    isRequired
-                                    label={intl.formatMessage({
-                                      id: 'new_code_definition.number_days.specify_days',
-                                    })}
-                                    max={90}
-                                    messageInvalid={
-                                      <FormattedMessage
-                                        id="new_code_definition.number_days.invalid"
-                                        values={{ 0: 1, 1: 90 }}
-                                      />
-                                    }
-                                    min={1}
-                                    onChange={handleDaysChange}
-                                    type="number"
-                                    validation={isValidDays(numberOfDays) ? 'none' : 'invalid'}
-                                    value={numberOfDays}
-                                    width={FormFieldWidth.Large}
+          <div className="sw-space-y-6">
+            <div>
+              <RadioButtonGroup
+                id="new-code-definition-type"
+                onChange={(value) => {
+                  handleTypeChange(value as NewCodeDefinitionType);
+                }}
+                options={[
+                  {
+                    label: intl.formatMessage({ id: 'new_code_definition.previous_version' }),
+                    value: NewCodeDefinitionType.PreviousVersion,
+                    helpText: (
+                      <>
+                        <div>
+                          <Text isSubtle size={TextSize.Small}>
+                            {intl.formatMessage({
+                              id: 'new_code_definition.previous_version.description',
+                            })}{' '}
+                            {intl.formatMessage(
+                              {
+                                id: 'new_code_definition.previous_version.usecase',
+                              },
+                              {
+                                b: (text) => (
+                                  <Text isHighlighted isSubtle size={TextSize.Small}>
+                                    {text}
+                                  </Text>
+                                ),
+                              },
+                            )}
+                          </Text>
+                        </div>
+
+                        <Divider className="sw-mt-4 sw-mb-2 -sw-ml-6" />
+                      </>
+                    ),
+                  },
+                  {
+                    label: intl.formatMessage({ id: 'new_code_definition.number_days' }),
+                    value: NewCodeDefinitionType.NumberOfDays,
+                    helpText: (
+                      <div className="sw-space-y-3">
+                        <Text isSubtle size={TextSize.Small}>
+                          {intl.formatMessage({
+                            id: 'new_code_definition.number_days.description',
+                          })}{' '}
+                          {intl.formatMessage(
+                            {
+                              id: 'new_code_definition.number_days.usecase',
+                            },
+                            {
+                              b: (text) => (
+                                <Text isHighlighted isSubtle size={TextSize.Small}>
+                                  {text}
+                                </Text>
+                              ),
+                            },
+                          )}
+                        </Text>
+
+                        {selectedType === NewCodeDefinitionType.NumberOfDays && (
+                          <div className="sw-space-y-2">
+                            <div className="sw-max-w-600">
+                              <TextInput
+                                isRequired
+                                label={intl.formatMessage({
+                                  id: 'new_code_definition.number_days.specify_days',
+                                })}
+                                max={90}
+                                messageInvalid={
+                                  <FormattedMessage
+                                    id="new_code_definition.number_days.invalid"
+                                    values={{ 0: 1, 1: 90 }}
                                   />
-                                </div>
-                                {shouldShowBanner && (
-                                  <MessageCallout
-                                    className="sw-mt-4"
-                                    onDismiss={dismissBanner}
-                                    variety={MessageVariety.Info}
-                                  >
-                                    <FormattedMessage
-                                      id="new_code_definition.auto_update.ncd_page.message"
-                                      values={{
-                                        previousDays: data?.previousNonCompliantValue,
-                                        days: numberOfDays,
-                                        date:
-                                          data?.updatedAt &&
-                                          new Date(data.updatedAt).toLocaleDateString(),
-                                        link: (
-                                          <Link
-                                            enableOpenInNewTab
-                                            highlight={LinkHighlight.CurrentColor}
-                                            to={docUrl(SharedDocLink.NewCodeDefinition)}
-                                          >
-                                            <FormattedMessage id="learn_more" />
-                                          </Link>
-                                        ),
-                                      }}
-                                    />
-                                  </MessageCallout>
-                                )}
-                              </div>
+                                }
+                                min={1}
+                                onChange={handleDaysChange}
+                                type="number"
+                                validation={isValidDays(numberOfDays) ? 'none' : 'invalid'}
+                                value={numberOfDays}
+                                width={FormFieldWidth.Large}
+                              />
+                            </div>
+
+                            {shouldShowBanner && (
+                              <MessageCallout
+                                className="sw-mt-4"
+                                onDismiss={dismissBanner}
+                                variety={MessageVariety.Info}
+                              >
+                                <FormattedMessage
+                                  id="new_code_definition.auto_update.ncd_page.message"
+                                  values={{
+                                    previousDays: data?.previousNonCompliantValue,
+                                    days: numberOfDays,
+                                    date:
+                                      data?.updatedAt &&
+                                      new Date(data.updatedAt).toLocaleDateString(),
+                                    link: (
+                                      <Link
+                                        enableOpenInNewTab
+                                        highlight={LinkHighlight.CurrentColor}
+                                        to={docUrl(SharedDocLink.NewCodeDefinition)}
+                                      >
+                                        <FormattedMessage id="learn_more" />
+                                      </Link>
+                                    ),
+                                  }}
+                                />
+                              </MessageCallout>
                             )}
                           </div>
-                        ),
-                      },
-                    ]}
-                    value={selectedType}
-                  />
-                </div>
-              </div>
+                        )}
+                      </div>
+                    ),
+                  },
+                ]}
+                value={selectedType}
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Change Notice and Actions */}
+        {hasChanged && (
+          <div>
+            <div className="sw-flex sw-items-center sw-gap-4">
+              <Button
+                className="js-submit-new-code"
+                isDisabled={isSaveButtonDisabled}
+                onClick={handleSubmit}
+                variety={ButtonVariety.Primary}
+              >
+                <FormattedMessage id="save" />
+              </Button>
+
+              <Button isDisabled={isPending || !hasChanged} onClick={handleCancel}>
+                <FormattedMessage id="cancel" />
+              </Button>
+
+              <Spinner isLoading={isPending} />
             </div>
 
-            {/* Change Notice and Actions */}
-            {hasChanged && (
-              <div>
-                <div className="sw-flex sw-items-center sw-gap-4">
-                  <Button
-                    className="js-submit-new-code"
-                    isDisabled={isSaveButtonDisabled}
-                    onClick={handleSubmit}
-                    variety={ButtonVariety.Primary}
-                  >
-                    <FormattedMessage id="save" />
-                  </Button>
-
-                  <Button isDisabled={isPending || !hasChanged} onClick={handleCancel}>
-                    <FormattedMessage id="cancel" />
-                  </Button>
-
-                  <Spinner isLoading={isPending} />
-                </div>
-                <MessageInline className="sw-mt-2" variety="info">
-                  <Text>
-                    <FormattedMessage id="new_code_definition.change_notice" />
-                  </Text>
-                </MessageInline>
-              </div>
-            )}
-          </form>
-        </Spinner>
-      </div>
-    </>
+            <MessageInline className="sw-mt-2" variety="info">
+              <Text>
+                <FormattedMessage id="new_code_definition.change_notice" />
+              </Text>
+            </MessageInline>
+          </div>
+        )}
+      </form>
+    </Spinner>
   );
 }
