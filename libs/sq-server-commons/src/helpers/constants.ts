3359908bff3c65e5de94bd49efa8d6807bbcfb65
Dorian Burihabwa <75226315+dorian-burihabwa-sonarsource@users.noreply.github.com>
2025-10-24 11:32:21 +0200

    SC-34839 Upgrade project onboarding tutorials to version 7.0.1.6134 of the Gradle scanner (#3625)
    GitOrigin-RevId: 6e4b8122d6e9fc8889059ef090e3f3dda4f9b32a

--- libs/sq-server-commons/src/helpers/constants.ts
+++ libs/sq-server-commons/src/helpers/constants.ts
@@ -313,7 +313,7 @@ export const IMPORT_COMPATIBLE_ALMS = [
   AlmKeys.GitLab,
 ];
 
-export const GRADLE_SCANNER_VERSION = '7.0.0.6105';
+export const GRADLE_SCANNER_VERSION = '7.0.1.6134';
 
 export const ONE_SECOND = 1000;
 
