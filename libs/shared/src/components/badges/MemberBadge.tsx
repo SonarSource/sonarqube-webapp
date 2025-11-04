18e8a158e4f6615efc2c6063a85b1107a0ace8de
Greg Aubert <gregoire.aubert@sonarsource.com>
2025-11-04 14:28:58 +0100

    SC-33636 Create new project sidebar with updated admin menu and project selector header (#3658)
    GitOrigin-RevId: 89f3f5bc83470d99abeb93841b59006d8b6426d5

---
+++ libs/shared/src/components/badges/MemberBadge.tsx
@@ -0,0 +1,34 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import { Badge, BadgeProps, BadgeVariety } from '@sonarsource/echoes-react';
+import { forwardRef } from 'react';
+import { FormattedMessage } from 'react-intl';
+
+type Props = Readonly<Pick<BadgeProps, 'className' | 'isInteractive'>>;
+
+export const MemberBadge = forwardRef<HTMLButtonElement, Props>((props, ref) => {
+  return (
+    <Badge variety={BadgeVariety.Neutral} {...props} ref={ref}>
+      <FormattedMessage id="member" />
+    </Badge>
+  );
+});
+MemberBadge.displayName = 'MemberBadge';
