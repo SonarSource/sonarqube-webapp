ae12fb27b92d18afa6073eb126e3b634a94a6d2c
Matt Pace <matt.pace@sonarsource.com>
2025-12-05 16:49:26 -0500

    SCA-1316 Fix dependency risks project filter on cloud (#3963)
    GitOrigin-RevId: f3fe1b43cec9d67ec91367477ac5c3a707aacb85

---
+++ libs/sq-server-commons/src/sq-server-adapters/api/components.ts
@@ -0,0 +1,47 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import { getTree } from '~shared/api/components';
+import { ComponentQualifier } from '~shared/types/component';
+import { Paging } from '~shared/types/paging';
+
+export async function getProjectsForComponent(params: {
+  componentKey: string;
+  componentQualifier: ComponentQualifier;
+  p?: number;
+  ps?: number;
+  q?: string;
+}): Promise<{ components: { key: string; name: string }[]; paging: Paging }> {
+  const { components, paging } = await getTree({
+    component: params.componentKey,
+    qualifiers: ComponentQualifier.Project,
+    p: params.p,
+    ps: params.ps,
+    q: params.q,
+  });
+
+  return {
+    components: components.map((component) => ({
+      key: component.refKey || component.key,
+      name: component.name,
+    })),
+    paging,
+  };
+}
