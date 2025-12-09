79119be8c80283f63ea4656af52e9823eba196fd
Ilia Kebets <ilia.kebets@sonarsource.com>
2025-12-09 17:13:16 +0100

    SONARCH-1704 Model: Warn user if constraints exist, that they will be deleted (#3989)
    GitOrigin-RevId: 3d7ae3864359685a21c88d8535e612564d267df2

--- libs/sq-server-commons/src/l10n/default.ts
+++ libs/sq-server-commons/src/l10n/default.ts
@@ -2050,6 +2050,8 @@ export const defaultMessages = {
     'Move the component and its code artifacts.',
   'architecture.page.model.modal.move_component.current_location': 'Current location:',
   'architecture.page.model.modal.move_component.new_location': 'New location:',
+  'architecture.page.model.modal.move_component.warning':
+    'When moving the component, all sibling relationships will be lost.',
   'architecture.page.model.modal.move_component_name': 'Component name',
   'architecture.page.model.modal.move_component_name_placeholder': 'Enter component name...',
   'architecture.page.model.modal.code_artifacts': 'Code Artifacts',
