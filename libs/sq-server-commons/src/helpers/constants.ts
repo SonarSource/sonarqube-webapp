e5ebe3b36765b1e3f53b4ff91d5c95125ad2fd84
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2026-01-09 18:10:17 +0100

    SC-38967 Revert Gradle scanner in project onboarding tutorials to version 7.1.0.6387
    GitOrigin-RevId: d84dfea1d300e900a32df73f7be929c264ab64d5

--- libs/sq-server-commons/src/helpers/constants.ts
+++ libs/sq-server-commons/src/helpers/constants.ts
@@ -313,7 +313,7 @@ export const IMPORT_COMPATIBLE_ALMS = [
   AlmKeys.GitLab,
 ];
 
-export const GRADLE_SCANNER_VERSION = '7.2.2.6593';
+export const GRADLE_SCANNER_VERSION = '7.1.0.6387';
 
 export const ONE_SECOND = 1000;
 
