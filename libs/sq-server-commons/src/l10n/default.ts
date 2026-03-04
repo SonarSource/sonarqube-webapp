a77cd90117a017236495cc93af746b04f464674b
Ilia Kebets <ilia.kebets@sonarsource.com>
2026-03-04 08:57:36 +0100

    SONARCH-2055 Should not move component with same name (#4731)
    GitOrigin-RevId: 27ed31622335583e706dd78d33f9f3344d82d72e

--- libs/sq-server-commons/src/l10n/default.ts
+++ libs/sq-server-commons/src/l10n/default.ts
@@ -2146,11 +2146,11 @@ export const defaultMessages = {
   'architecture.page.model.modal.move_component.disabled_duplicate':
     'This component has a child with the same name.',
   'architecture.page.model.modal.move_component.disabled_current_location':
-    'You cannot move a component to its own location.',
+    'You cannot move this component to its own location.',
   'architecture.page.model.modal.move_component.disabled_selected':
-    'You cannot move a component to itself.',
+    'You cannot move this component to itself.',
   'architecture.page.model.modal.move_component.disabled_kind':
-    'You cannot move a component to {kind}.',
+    'You cannot move this component to {kind}.',
   'architecture.page.model.modal.rename_component.title': 'Rename "{label}"',
   'architecture.page.model.modal.rename_component.new_name': 'New name',
   'architecture.page.model.modal.code_artifacts': 'Code Artifacts',
