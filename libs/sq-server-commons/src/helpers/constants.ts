a74f64ac35016413ce0b7cd42d6cac49a629e431
Aurélien Coet <aurelien.coet@sonarsource.com>
2026-04-30 11:58:55 +0200

    SC-47521 Upgrade project onboarding tutorials to use version 7.3.0.8198 of the Gradle scanner (#5373)
    GitOrigin-RevId: a18767f51c1cf88fb95939af062d10a66c6268f1

--- libs/sq-server-commons/src/helpers/constants.ts
+++ libs/sq-server-commons/src/helpers/constants.ts
@@ -308,7 +308,7 @@ export const IMPORT_COMPATIBLE_ALMS = [
   AlmKeys.GitLab,
 ];
 
-export const GRADLE_SCANNER_VERSION = '7.2.3.7755';
+export const GRADLE_SCANNER_VERSION = '7.3.0.8198';
 
 export const ONE_SECOND = 1000;
 
