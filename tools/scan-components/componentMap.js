5d9ed4f76f40ba1671a8fe31d3a6c8da4e48bdcd
Quentin Chevrin <110476765+quentin-chevrin-sonarsource@users.noreply.github.com>
2026-06-01 12:01:20 +0200

    SC-45836 Remove downgrade feedback survey (#5787)
    GitOrigin-RevId: 77b9178900c2e16460bead01fcbcef290a166d38

--- tools/scan-components/componentMap.js
+++ tools/scan-components/componentMap.js
@@ -2027,7 +2027,6 @@ export const componentMap = {
   GraphsLegendStatic: 'Excluded',
   OrganizationLongLivedBranchesPattern: 'Excluded',
   SettingsSearchOverlay: 'Excluded',
-  DowngradeFeedback: 'Excluded',
   MetaOrganizationKey: 'Excluded',
   EnterprisePermissionsIntro: 'Excluded',
   PortfolioBreakdownTable: 'Excluded',
